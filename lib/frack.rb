368c68f70edd8d2031e0e36382e35e2ccd6fa507
ngocson <ngocson13btt@gmail.com>
2019-11-17 22:24:11 +0700

    create lib

---
+++ lib/frack.rb
@@ -0,0 +1,12 @@
+$LOAD_PATH << File.expand_path(File.dirname(__FILE__))
+require 'rack'
+require 'tilt'
+
+module Frack
+  autoload :Application, 'frack/application'
+  autoload :BaseController, 'frack/base_controller'
+end
+
+require 'app/models/user'
+require 'app/controllers/users_controller'
+require 'app/controllers/welcome_controller'
---
+++ lib/frack/application.rb
@@ -0,0 +1,13 @@
+class Application
+  class << self
+    def call(env)
+      if env['PATH_INFO'] == '/'
+        Rack::Response.new(WelcomeController.new.index)
+      elsif env['PATH_INFO'] == '/users'
+        Rack::Response.new(UsersController.new.index)
+      else
+        Rack::Response.new('Not Found', 404)
+      end
+    end
+  end
+end
