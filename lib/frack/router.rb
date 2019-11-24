972fdf4a1db276b1667030c4492c961848c89de8
ngocson <ngocson13btt@gmail.com>
2019-11-24 21:16:20 +0700

    fix  router

---
+++ lib/frack/router.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+module Frack
+  class Router
+    attr_reader :app
+    ROUTES = {
+      '/' =>  'WelcomeController#index',
+      '/users' => 'UsersController#index'
+    }
+    def initialize(app)
+        @app= app
+    end
+
+    def call(env)
+      if (mapping = ROUTES[env['PATH_INFO']])
+        env.merge!(controller_action(mapping))
+        app.call(env)
+      else
+        Rack::Response.new('Not found', 404)
+    end
+    def controller_action(mapping)
+      Hash[ %w(controller action).zip mapping.split('#') ]
+    end
+  end
+end
