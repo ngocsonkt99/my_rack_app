5a8f31966290260163c335c6e76273595646dfa5
ngocson <ngocson13btt@gmail.com>
2020-01-05 20:19:45 +0700

    refactor file router

--- lib/frack/router.rb
+++ lib/frack/router.rb
@@ -4,23 +4,43 @@ module Frack
   class Router
     attr_reader :app
 
-    ROUTES = {
-      '/' => 'WelcomeController#index',
-      '/users' => 'UsersController#index',
-      '/sign_up' => 'UsersController#new'
-    }
+    ROUTES = [
+      {
+        request_method: 'get',
+        request_path: '/',
+        mapping: 'WelcomeController#new'
+      },
+      {
+        request_method: 'get',
+        request_path: '/sign_up',
+        mapping: 'UsersController#new'
+      },
+      {
+        request_method: 'post',
+        request_path: '/sign_up',
+        mapping: 'UsersController#create'
+      }
+    ].freeze
+
+    def call(env)
+      request_method = env['REQUEST_METHOD'].downcase
+      request_path = env['PATH_INFO']
+
+      route = ROUTES.find do |i|
+        i[:request_method] == request_method &&
+       i[:request_path] == request_path
+     end
+
+     if (mapping = ROUTES[env['PATH_INFO']])
+       env.merge!(controller_action(mapping))
+       @app.call(env)
+     else
+       Rack::Response.new('Not found',404)
+     end
+   end
 
     def initialize(app)
       @app = app
-    end
-
-    def call(env)
-      if (mapping = ROUTES[env['PATH_INFO']])
-        env.merge!(controller_action(mapping))
-        app.call(env)
-      else
-        Rack::Response.new('Not found', 404)
-      end
     end
 
     def controller_action(mapping)
